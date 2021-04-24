a225ed2317a690704ae4ee74646eafff038e5a7f
Ahmed Yehia <ahmedyehia.edu@gmail.com>
2021-04-24 18:20:12 +0200

    fixing minor bug

--- routes/students.js
+++ routes/students.js
@@ -2,8 +2,7 @@ const router = require("express").Router();
 const controller = require("../controllers/students");
 const fs = require("fs")
 
-data = JSON.parse(fs.readFileSync("./data.json",'utf-8', err => {if(err) throw err}))
-
+// full path is used here so that endpoint variable is avalible inside callback
 router.post("/:endpoint/students", controller.createStudent)         // C
 router.get('/:endpoint/students/:id?', controller.getStudent)        // R
 router.put("/:endpoint/students/:id", controller.updateStudents)     // U
